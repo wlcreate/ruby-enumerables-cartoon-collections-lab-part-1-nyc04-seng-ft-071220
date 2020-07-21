bba4c4282d89f06fb99f3730302fd1412b9f332a
Waverley Leung <waverley.nyc@gmail.com>
2020-07-21 15:50:08 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,10 +2,19 @@ def greet_characters(array)
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
+  total = 0
+  array.each do |element|
+    puts "Hello #{element}!"
+    total += 1
+  end
 end
 
 def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
+  array.each_with_index do |element, index|
+    index += 1
+    puts "#{index}. #{element}"
+  end
 end
